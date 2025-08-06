5e52cc9922d9708f57ac238b9d92607f40600e14
Caden Austin <cadenbryant@hotmail.com>
2025-08-06 16:11:39 -0400

    Write Functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read, net::Ipv4Addr, os::raw};
+use std::{fs::File, io::Read, net::{Ipv4Addr, UdpSocket}, os::raw};
 
 pub struct BytePacketBuffer {
     pub buf: [u8; 512],
@@ -39,11 +39,26 @@ impl BytePacketBuffer {
         Ok(res)
     }
 
+    fn write(&mut self, val: u8) -> Result<(), String> {
+        if self.pos >= 512 {
+            return Err("Buffer overflow".into());
+        }
+        self.buf[self.pos] = val;
+        self.pos += 1;
+        Ok(())
+    }
+
     fn get(&mut self, pos: usize) -> Result<u8, String> {
         if pos >= self.buf.len() {
             return Err("Index out of bounds".into());
         }
         Ok(self.buf[pos])
+    }
+
+    fn write_u8(&mut self, val: u8) -> Result<(), ()> {
+        self.write(val).unwrap();
+
+        Ok(())
     }
 
     fn get_range(&mut self, start: usize, len: usize) -> Result<&[u8], String> {
@@ -59,27 +74,54 @@ impl BytePacketBuffer {
         Ok((high as u16) << 8 | (low as u16))
     }
 
+    fn write_u16(&mut self, val: u16) -> Result<(), ()> {
+        self.write((val >> 8) as u8).unwrap();
+        self.write((val & 0xFF) as u8).unwrap();
+        Ok(())
+    }
+
     fn read_u32(&mut self) -> Result<u32, ()> {
-        let high = self.read_u16().unwrap();
-        let low = self.read_u16().unwrap();
-        Ok((high as u32) << 16 | (low as u32))
+        let res = ((self.read().unwrap() as u32) << 24)
+            | ((self.read().unwrap() as u32) << 16)
+            | ((self.read().unwrap() as u32) << 8)
+            | ((self.read().unwrap() as u32) << 0);
+
+        Ok(res)
+    }
+
+    fn write_u32(&mut self, val: u32) -> Result<(), ()> {
+        self.write(((val >> 24) & 0xFF) as u8).unwrap();
+        self.write(((val >> 16) & 0xFF) as u8).unwrap();
+        self.write(((val >> 8) & 0xFF) as u8).unwrap();
+        self.write(((val >> 0) & 0xFF) as u8).unwrap();
+
+        Ok(())
     }
 
     fn read_qname(&mut self, outstr: &mut String) -> Result<(), String> {
         let mut pos = self.pos();
         let mut jumped = false;
+
+        let mut delim = "";
         let max_jumps = 5;
         let mut jumps_performed = 0;
-        let mut delim = "";
-
         loop {
-            if jumps_performed >= max_jumps {
-                return Err(format!("Limit of {} jumps exceeded", max_jumps));
+            // Dns Packets are untrusted data, so we need to be paranoid. Someone
+            // can craft a packet with a cycle in the jump instructions. This guards
+            // against such packets.
+            if jumps_performed > max_jumps {
+                return Err(format!("Limit of {} jumps exceeded", max_jumps).into());
             }
 
             let len = self.get(pos)?;
 
+            // A two byte sequence, where the two highest bits of the first byte is
+            // set, represents a offset relative to the start of the buffer. We
+            // handle this by jumping to the offset, setting a flag to indicate
+            // that we shouldn't update the shared buffer position once done.
             if (len & 0xC0) == 0xC0 {
+                // When a jump is performed, we only modify the shared buffer
+                // position once, and avoid making the change later on.
                 if !jumped {
                     self.seek(pos + 2).unwrap();
                 }
@@ -87,29 +129,49 @@ impl BytePacketBuffer {
                 let b2 = self.get(pos + 1)? as u16;
                 let offset = (((len as u16) ^ 0xC0) << 8) | b2;
                 pos = offset as usize;
-
                 jumped = true;
                 jumps_performed += 1;
-
                 continue;
-            } else {
-                pos += 1;
-                if len == 0 {
-                    break;
-                }
-                outstr.push_str(delim);
-                let str_buffer = self.get_range(pos, len as usize).unwrap();
-                outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
-
-                delim = ".";
-                pos += len as usize
             }
+
+            pos += 1;
+
+            // Names are terminated by an empty label of length 0
+            if len == 0 {
+                break;
+            }
+
+            outstr.push_str(delim);
+
+            let str_buffer = self.get_range(pos, len as usize).unwrap();
+            outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+
+            delim = ".";
+
+            pos += len as usize;
         }
 
         if !jumped {
             self.seek(pos).unwrap();
         }
 
+        Ok(())
+    }
+
+    fn write_qname(&mut self, qname: &str) -> Result<(), String> {
+        for label in qname.split('.') {
+            let len = label.len();
+            if len > 0x3f {
+                return Err("Single label exceeds 63 characters of length".into());
+            }
+
+            self.write_u8(len as u8).unwrap();
+            for b in label.as_bytes() {
+                self.write_u8(*b).unwrap();
+            }
+        }
+
+        self.write_u8(0).unwrap();
         Ok(())
     }
 }
@@ -208,11 +270,38 @@ impl DnsHeader {
 
         Ok(())
     }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<(), ()> {
+        buffer.write_u16(self.id)?;
+
+        buffer.write_u8(
+            (self.recursion_desired as u8)
+                | ((self.truncated_message as u8) << 1)
+                | ((self.authoritative_answer as u8) << 2)
+                | (self.opcode << 3)
+                | ((self.response as u8) << 7) as u8,
+        )?;
+
+        buffer.write_u8(
+            (self.rescode as u8)
+                | ((self.checking_disabled as u8) << 4)
+                | ((self.authed_data as u8) << 5)
+                | ((self.z as u8) << 6)
+                | ((self.recursion_available as u8) << 7),
+        )?;
+
+        buffer.write_u16(self.questions)?;
+        buffer.write_u16(self.answers)?;
+        buffer.write_u16(self.authoritative_entries)?;
+        buffer.write_u16(self.resource_entries)?;
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum QueryType {
-    UNKOWN(u16),
+    UNKNOWN(u16),
     A,
     
 }
@@ -220,7 +309,7 @@ pub enum QueryType {
 impl QueryType {
     pub fn to_u16(&self) -> u16 {
         match *self {
-            QueryType::UNKOWN(v) => v,
+            QueryType::UNKNOWN(v) => v,
             QueryType::A => 1,
         }
     }
@@ -228,7 +317,7 @@ impl QueryType {
     pub fn from_u16(value: u16) -> QueryType {
         match value {
             1 => QueryType::A,
-            _ => QueryType::UNKOWN(value),
+            _ => QueryType::UNKNOWN(value),
         }
     }
 }
@@ -250,14 +339,23 @@ impl DnsQuestion {
 
         let _class = buffer.read_u16().unwrap(); // Class, TODO
         Ok(())
+    }
 
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<(), ()> {
+        buffer.write_qname(&self.name).unwrap();
+        
+        let typenum = self.qtype.to_u16();
+        buffer.write_u16(typenum).unwrap();
+        buffer.write_u16(1).unwrap();
+
+        Ok(())
     }
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(dead_code)]
 pub enum DnsRecord {
-    UNKOWN {
+    UNKNOWN {
         domain: String,
         qtype: QueryType,
         data_len: u16,
@@ -291,13 +389,42 @@ impl DnsRecord {
                 );
                 Ok(DnsRecord::A { domain, addr, ttl })
             }
-            _ => Ok(DnsRecord::UNKOWN {
+            _ => Ok(DnsRecord::UNKNOWN {
                 domain,
                 qtype,
                 data_len,
                 ttl,
             }),
         }
+    }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<usize, ()> {
+        let start_pos = buffer.pos();
+
+        match *self {
+            DnsRecord::A {
+                ref domain,
+                ref addr,
+                ttl,
+            } => {
+                buffer.write_qname(domain).unwrap();
+                buffer.write_u16(QueryType::A.to_u16()).unwrap();
+                buffer.write_u16(1).unwrap();
+                buffer.write_u32(ttl).unwrap();
+                buffer.write_u16(4).unwrap();
+
+                let octets = addr.octets();
+                buffer.write_u8(octets[0]).unwrap();
+                buffer.write_u8(octets[1]).unwrap();
+                buffer.write_u8(octets[2]).unwrap();
+                buffer.write_u8(octets[3]).unwrap();
+            }
+            DnsRecord::UNKNOWN { .. } => {
+                println!("Skipping record: {:?}", self);
+            }
+        }
+
+        Ok(buffer.pos() - start_pos)
     }
 }
 
@@ -326,7 +453,7 @@ impl DnsPacket {
         result.header.read(buffer).unwrap();
 
         for _ in 0..result.header.questions {
-            let mut question = DnsQuestion::new(String::new(), QueryType::UNKOWN(0));
+            let mut question = DnsQuestion::new(String::new(), QueryType::UNKNOWN(0));
             question.read(buffer).unwrap();
             result.questions.push(question);
         }
@@ -348,26 +475,65 @@ impl DnsPacket {
 
         Ok(result)
     }
+
+    pub fn write(&mut self, buffer: &mut BytePacketBuffer) -> Result<(), ()> {
+        self.header.questions = self.questions.len() as u16;
+        self.header.answers = self.answers.len() as u16;
+        self.header.authoritative_entries = self.authorities.len() as u16;
+        self.header.resource_entries = self.resources.len() as u16;
+
+        self.header.write(buffer).unwrap();
+
+        for question in &self.questions {
+            question.write(buffer).unwrap();
+        }
+        for rec in &self.answers {
+            rec.write(buffer).unwrap();
+        }
+        for rec in &self.authorities {
+            rec.write(buffer).unwrap();
+        }
+        for rec in &self.resources {
+            rec.write(buffer).unwrap();
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), String> {
-    let mut f = File::open("response_packet.txt").expect("Unable to open file");
-    let mut buffer = BytePacketBuffer::new();
-    f.read(&mut buffer.buf).expect("Unable to read file");
+    let qname = "google.com";
+    let qtype = QueryType::A;
 
-    let packet = DnsPacket::from_buffer(&mut buffer).expect("Failed to parse DNS packet");
-    println!("{:#?}", packet.header);
+    let server = ("8.8.8.8", 53);
+    let socket = UdpSocket::bind(("0.0.0.0", 43210)).unwrap();
+    let mut packet = DnsPacket::new();
 
-    for q in packet.questions {
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
+
+    let mut req_buffer = BytePacketBuffer::new();
+    packet.write(&mut req_buffer).unwrap();
+    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server).unwrap();
+    
+    let mut res_buffer = BytePacketBuffer::new();
+    socket.recv_from(&mut res_buffer.buf).unwrap();
+    let res_packet = DnsPacket::from_buffer(&mut res_buffer).unwrap();
+    
+    println!("{:#?}", res_packet.header);
+
+    for q in res_packet.questions {
         println!("{:#?}", q);
     }
-    for rec in packet.answers {
+    for rec in res_packet.answers {
         println!("{:#?}", rec);
     }
-    for rec in packet.authorities {
+    for rec in res_packet.authorities {
         println!("{:#?}", rec);
     }
-    for rec in packet.resources {
+    for rec in res_packet.resources {
         println!("{:#?}", rec);
     }
 
